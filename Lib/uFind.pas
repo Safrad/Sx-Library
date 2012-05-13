77543468f9ef488fe14e51f7b8f6b4004852a522
safrada <safrad@users.noreply.github.com>
2012-05-13 18:18:33 +0000

    Added LevenshteinDistance.

--- Lib/uFind.pas
+++ Lib/uFind.pas
@@ -16,12 +16,15 @@ function FindS(AValue: array of string;
 (*
 function FindBM(SubStr, Str: string): SG;
 function FindKMP(SubStr, Str: string): SG;
-function Search(Pattern, Text: string; ErrorLen: SG = 0): SG; // 10x slower that Pos();
 *)
+function SearchHamming(const Pattern, Text: string; ErrorLen: SG = 0): SG; // 10x slower that Pos();
+function LevenshteinDistance(const A1, A2: string; const ACostInsert: integer = 1; const ACostDel: integer = 1; const ACostRep: integer = 1): integer;
 
 implementation
 
-uses uStrings, SysUtils, uCharset;
+uses
+  Math,
+	uStrings, SysUtils, uCharset;
 
 function FindS2(AValue: PArrayS2; var FromV, ToV: SG; const Value: S2; FindGroup: BG): Boolean;
 {$I Find.inc}
@@ -257,8 +260,9 @@ begin
 			j := j + Max(Fail1[Ord(Str[j])], Fail2[i]);
 	end;
 end;
+*)
 
-function Search(Pattern, Text: string; ErrorLen: SG = 0): SG;
+function SearchHamming(const Pattern, Text: string; ErrorLen: SG = 0): SG;
 const
 	Empty = $FFFFFFFFFFFFFFFF;
 var
@@ -297,7 +301,7 @@ begin
 		begin
 			R[j] :=
 				((R[j] shl 1) or D[Text[i]]) and // Hamming
-				// ((R[j - 1] and LR[j - 1])shl 1) and // Levenshtein
+				//((R[j - 1] and LR[j - 1])shl 1) and // Levenshtein
 				(R[j - 1] shl 1 {or V});
 		end;
 		R[0] := (R[0] shl 1) or D[Text[i]];
@@ -315,6 +319,64 @@ begin
 
 	end;
 end;
-*)
+
+function LevenshteinDistance(const A1, A2: string; const ACostInsert: integer = 1; const ACostDel: integer = 1; const ACostRep: integer = 1): integer;
+var
+  ari1, ari2, aris: PIntegerArray;
+  i1: integer;
+  i2: integer;
+  iC0: integer;
+  iC1: integer;
+  iCount: integer;
+  iCount2: integer;
+begin
+  i1 := length(A1);
+  i2 := length(A2);
+  if i1 = 0 then
+  begin
+    Result := i2 * ACostInsert;
+    exit;
+  end;
+  if i2 = 0 then
+  begin
+    Result := i1 * ACostDel;
+    exit;
+  end;
+
+  GetMem(ari1, sizeof(integer) * (i2 + 1));
+  try
+    for iCount := 0 to i2 do
+      ari1[iCount] := iCount * ACostInsert;
+
+    GetMem(ari2, sizeof(integer) * (i2 + 1));
+    try
+      for iCount := 0 to i1-1 do
+      begin
+        ari2[0] := ari1[0] + ACostDel;
+        for iCount2 := 0 to i2-1 do
+        begin
+          iC0 := ari1[iCount2];
+          if A1[iCount + 1] <> A2[iCount2 + 1] then
+            iC0 := iC0 + ACostRep;
+          iC1 := ari1[iCount2 + 1] + ACostDel;
+          if iC1 < iC0 then
+            iC0 := iC1;
+          iC1 := ari2[iCount2] + ACostInsert;
+          if iC1 < iC0 then
+            iC0 := iC1;
+          ari2[iCount2 + 1] := iC0;
+        end;
+        aris := ari1;
+        ari1 := ari2;
+        ari2 := aris;
+      end;
+    finally
+      FreeMem(ari2);
+    end;
+    Result := ari1[i2];
+  finally
+    FreeMem(ari1);
+  end;
+end;
 
 end.
