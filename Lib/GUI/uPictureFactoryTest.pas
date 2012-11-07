b143421254175f9b478a4f9fbb03d2a0336593c9
safrada <safrad@users.noreply.github.com>
2012-11-07 11:28:11 +0000

    Added unit tests.

---
+++ Lib/GUI/uPictureFactoryTest.pas
@@ -0,0 +1,47 @@
+unit uPictureFactoryTest;
+
+interface
+
+uses TestFrameWork;
+
+type
+  TPictureFactoryTest = class(TTestCase)
+  published
+    procedure Test;
+  end;
+
+implementation
+
+uses
+  uPictureFactory,
+  uDBitmap,
+  uFiles;
+
+{ TObjectFactoryTest }
+
+procedure TPictureFactoryTest.Test;
+var
+  PictureFactory: TPictureFactory;
+  Bmp: TDBitmap;
+begin
+  PictureFactory := TPictureFactory.Create;
+  try
+    PictureFactory.Paths.Add(GraphDir);
+    Bmp := PictureFactory.GetBitmap('my');
+    Check(Bmp = nil);
+    Bmp := PictureFactory.GetBitmap('my');
+    Check(Bmp = nil);
+    Bmp := PictureFactory.GetBitmap('Lib');
+    Check(Bmp <> nil);
+    Bmp := PictureFactory.GetBitmap('Lib');
+    Check(Bmp <> nil);
+    Bmp := PictureFactory.GetBitmap('SxLib');
+    Check(Bmp <> nil);
+  finally
+    PictureFactory.Free;
+  end;
+end;
+
+initialization
+	RegisterTest('Picture Factory Test', TPictureFactoryTest.Suite);
+end.
