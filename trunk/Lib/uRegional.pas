b18af2971e7c6ac171900255ab862e70dccb3a9d
safrada@gmail.com <safrada@gmail.com@4efd7948-4572-46e3-a476-6112b4b41e88>
2012-12-14 11:46:35 +0000

    Added feast days.

--- trunk/Lib/uRegional.pas
+++ trunk/Lib/uRegional.pas
@@ -2,13 +2,22 @@ unit uRegional;
 
 interface
 
+uses uTypes;
+
 function DateToCS(const D: TDateTime): string; // Birthday, Players desription
 function DateToCSLong(const D: TDateTime): string; // Chart legend
 function DateToCSShort(const D: TDateTime): string; // Events
 
+function IsNewYear(const ADate: TDate): BG;
+function IsChristmas(const ADate: TDate): BG;
+
+function IsNewYearOnStartup: BG;
+function IsChristmasOnStartup: BG;
+
 implementation
 
-uses SysUtils;
+uses
+  SysUtils, DateUtils;
 
 function DateToCS(const D: TDateTime): string; // Birthday, Players desription
 begin
@@ -34,5 +43,35 @@ begin
 		DateTimeToString(Result, 'd.m.', D);
 end;
 
+function IsNewYear(const ADate: TDate): BG;
+var Day, Month, Year: U2;
+begin
+  DecodeDate(ADate, Year, Month, Day);
+  Result := (Day = 1) and (Month = 1);
+end;
+
+function IsChristmas(const ADate: TDate): BG;
+var Day, Month, Year: U2;
+begin
+  DecodeDate(ADate, Year, Month, Day);
+  Result := (Day = 24) and (Month = 12);
+end;
+
+var
+  GIsChristmasOnStartup: BG;
+  GIsNewYearOnStartup: BG;
+
+function IsNewYearOnStartup: BG;
+begin
+  Result := GIsNewYearOnStartup;
+end;
+
+function IsChristmasOnStartup: BG;
+begin
+  Result := GIsChristmasOnStartup;
+end;
+
+initialization
+  GIsNewYearOnStartup := IsNewYear(Date);
+  GIsChristmasOnStartup := IsChristmas(Date);
 end.
- 
