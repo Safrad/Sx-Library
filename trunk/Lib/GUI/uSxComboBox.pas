0c22ba1580ae1236932529c9b9c777d100217f0a
safrada@gmail.com <safrada@gmail.com@4efd7948-4572-46e3-a476-6112b4b41e88>
2013-02-07 17:08:41 +0000

    Added SxComboBox.

---
+++ trunk/Lib/GUI/uSxComboBox.pas
@@ -0,0 +1,56 @@
+unit uSxComboBox;
+
+interface
+
+uses
+  uTypes,
+  StdCtrls;
+
+type
+  TSxComboBox = class(TComboBox)
+  public
+    procedure SetText(const AText: string);
+    procedure SetItemIndex(const AItemIndex: SG);
+  end;
+
+procedure Register;
+
+implementation
+
+uses
+  Classes;
+
+procedure Register;
+begin
+	RegisterComponents(ComponentPageName, [TSxComboBox]);
+end;
+
+{ TSxComboBox }
+
+procedure TSxComboBox.SetItemIndex(const AItemIndex: SG);
+var
+  LastOnChange: TNotifyEvent;
+begin
+  LastOnChange := OnChange;
+  try
+    OnChange := nil;
+    ItemIndex := AItemIndex;
+  finally
+    OnChange := LastOnChange;
+  end;
+end;
+
+procedure TSxComboBox.SetText(const AText: string);
+var
+  LastOnChange: TNotifyEvent;
+begin
+  LastOnChange := OnChange;
+  try
+    OnChange := nil;
+    Text := AText;
+  finally
+    OnChange := LastOnChange;
+  end;
+end;
+
+end.
