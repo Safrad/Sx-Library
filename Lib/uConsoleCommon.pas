7d91af30ce5476d4e8dbb54a3d98545ff024d790
safrada <safrad@users.noreply.github.com>
2013-04-17 18:57:19 +0000

    Added common code for console application.

---
+++ Lib/uConsoleCommon.pas
@@ -0,0 +1,57 @@
+{
+	Usability in "Project file" (*.dpr):
+
+begin
+	CommonCreate;
+
+  ...
+
+	CommonFree;
+end.
+}
+
+unit uConsoleCommon;
+
+interface
+
+uses
+  uTypes;
+
+procedure CommonCreate(const ProductVersion: string; const OutputInfo: BG = True);
+procedure CommonFree;
+
+implementation
+
+uses
+  SysUtils,
+  uLog, uFiles, uDIniFile, uStart, uUsageInfo,
+  uProjectInfo;
+
+procedure CommonCreate(const ProductVersion: string; const OutputInfo: BG = True);
+begin
+  InitializeLog;
+
+  ThisProjectInfo.SetProjectInfo(piProductName, ThisProjectInfo.GetProjectInfo(piInternalName));
+  ThisProjectInfo.SetProjectInfo(piProductVersion, ProductVersion);
+  ThisProjectInfo.SetProjectInfo(piFileVersion, ProductVersion);
+
+  if OutputInfo then
+    Writeln(GetProjectInfo(piProductName) + ' [Version ' + GetProjectInfo(piFileVersion) + ']');
+
+	MainIni := TDIniFile.Create(MainIniFileName);
+	LocalMainIni := TDIniFile.Create(LocalIniFileName);
+
+  RWStart(MainIni, False);
+  TryUploadData;
+end;
+
+procedure CommonFree;
+begin
+  RWStart(MainIni, True);
+
+	FreeAndNil(MainIni);
+	FreeAndNil(LocalMainIni);
+	FreeAndNil(MainLog);
+end;
+
+end.
