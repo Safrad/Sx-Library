adbbbef2a91421fc8d18049835002190de1ce0db
safrada@gmail.com <safrada@gmail.com@4efd7948-4572-46e3-a476-6112b4b41e88>
2014-06-17 20:55:01 +0000

    Pause if errorlevel <> 0.

--- trunk/autoconf.cmd
+++ trunk/autoconf.cmd
@@ -3,9 +3,9 @@ rem run before Delphi IDE starts
 rem set user environment variable sx-library to current path
 setx sx-library "%cd%"
 
-@if not errorlevel 0 pause
+@IF NOT %ERRORLEVEL% EQU 0 PAUSE
 
 rem set user environment variable ifer to current path
 PathsToReg.exe
 
-@if not errorlevel 0 pause
+@IF NOT %ERRORLEVEL% EQU 0 PAUSE
