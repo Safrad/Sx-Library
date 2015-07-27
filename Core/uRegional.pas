6e205d6e6dbf5ca4847f2cf2371ccddd174c5b78
safrad <safrad@4efd7948-4572-46e3-a476-6112b4b41e88>
2015-07-27 11:13:15 +0000

    Add IsPublicHolidayDay.

--- Core/uRegional.pas
+++ Core/uRegional.pas
@@ -10,6 +10,7 @@ function DateToCSShort(const D: TDateTime): string; // Events
 
 function IsNewYear(const ADate: TDate): BG;
 function IsChristmas(const ADate: TDate): BG;
+function IsPublicHolidayDay(const Date: TDateTime): BG;
 
 function IsNewYearOnStartup: BG;
 function IsChristmasOnStartup: BG;
@@ -69,6 +70,56 @@ end;
 function IsChristmasOnStartup: BG;
 begin
   Result := GIsChristmasOnStartup;
+end;
+
+function IsPublicHolidayDay(const Date: TDateTime): BG;
+var
+  Day, Month, Year: U2;
+begin
+  Result := False; // TODO
+  DecodeDate(Date, Year, Month, Day);
+  case Month of
+  1:
+  begin
+    if Day in [1] then
+      Result := True;
+  end;
+  4:
+  begin
+    // TODO : Easter
+  end;
+  5:
+  begin
+    if Day in [1, 8] then
+      Result := True;
+  end;
+  7:
+  begin
+    if Day in [5, 6] then
+      Result := True;
+  end;
+  9:
+  begin
+    if Day in [28] then
+      Result := True;
+  end;
+  10:
+  begin
+    if Day in [28] then
+      Result := True;
+  end;
+  11:
+  begin
+    if Day in [17] then
+      Result := True;
+  end;
+  12:
+  begin
+    if Day in [24, 25, 26] then
+      Result := True;
+  end;
+  end;
+
 end;
 
 initialization
