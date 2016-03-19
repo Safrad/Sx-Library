3acd2847f043570ebff8e0f09827b035273c0f93
David Šafránek <safrad@email.cz>
2016-03-19 21:11:55 +0100

    Fix integer overflow.

--- Core/uMath.pas
+++ Core/uMath.pas
@@ -1479,7 +1479,7 @@ var
   d1, d2: SG;
 begin
   P1 := PSG(@Desc);
-  P2 := PSG(SG(@Desc) + SizeOf(SG) * (Size - 1));
+  P2 := PSG(SG(@Desc) + SizeOf(SG) * SG(Size - 1));
   for i := 0 to Size div 2 - 1 do
   begin
     d1 := P1^;
