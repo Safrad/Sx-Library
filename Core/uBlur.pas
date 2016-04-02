b0753cedd47acb0ec01f712119d570a1a590cb75
David Šafránek <safrad@email.cz>
2016-04-02 16:14:33 +0200

    Use FG instead of F8.

--- Core/uBlur.pas
+++ Core/uBlur.pas
@@ -2,51 +2,52 @@ unit uBlur;
 
 interface
 
-uses uTypes;
+uses
+  uTypes;
 
 type
-	// TFlo = F4;
-	TFlo = F8;
-	// TFlo = FA;
+  TFlo = FG;
 
-	PPointDS = ^TPointDS;
+  PPointDS = ^TPointDS;
 
-	TPointDS = record
-		X, Y: TFlo;
-		Selected: BG;
-	end;
+  TPointDS = record
+    X, Y: TFlo;
+    Selected: BG;
+  end;
 
-	PVector = ^TVector;
+  PVector = ^TVector;
 
-	TVector = record
-		S: TPointDS;
+  TVector = record
+    S: TPointDS;
 //		M: TPointDS;
-		E: TPointDS;
-	end;
+    E: TPointDS;
+  end;
 
-	PBlur = ^TBlur;
-	TBlur = record
-		Intensity: TFlo;
-		Direction: TFlo;
-		x, y: TFlo;
+  PBlur = ^TBlur;
+
+  TBlur = record
+    Intensity: TFlo;
+    Direction: TFlo;
+    x, y: TFlo;
 //		Fixed: BG;
-	end;
+  end;
 
-	TBlurArray = class
-		FData: PBlur;
-		FWidth: UG;
-		FHeight: UG;
-	public
-		constructor Create;
-		destructor Destroy; override;
-		procedure SetSize(const Width, Height: UG);
-		function GetAddr(const x, y: UG): PBlur;
-		function GetAddrChecked(const x, y: SG): PBlur;
-		property Data: PBlur read FData;
-	end;
+  TBlurArray = class
+    FData: PBlur;
+    FWidth: UG;
+    FHeight: UG;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure SetSize(const Width, Height: UG);
+    function GetAddr(const x, y: UG): PBlur;
+    function GetAddrChecked(const x, y: SG): PBlur;
+    property Data: PBlur read FData;
+  end;
 
 function GetLength(const FloPoint: TFloPoint; const P: TPointDS): TFlo; overload;
-function GetLength(const X, Y: SG; const P: TPointDS): TFlo;  overload;
+
+function GetLength(const X, Y: SG; const P: TPointDS): TFlo; overload;
 
 implementation
 
@@ -54,50 +55,50 @@ implementation
 
 constructor TBlurArray.Create;
 begin
-	inherited;
-	FData := nil;
+  inherited;
+  FData := nil;
 end;
 
 destructor TBlurArray.Destroy;
 begin
-	FreeMem(FData);
-	FData := nil;
-	inherited;
+  FreeMem(FData);
+  FData := nil;
+  inherited;
 end;
 
 function TBlurArray.GetAddr(const x, y: UG): PBlur;
 begin
-	Result := PBlur(UG(FData) + (y * FWidth + x) * SizeOf(TBlur));
+  Result := PBlur(UG(FData) + (y * FWidth + x) * SizeOf(TBlur));
 end;
 
 function TBlurArray.GetAddrChecked(const x, y: SG): PBlur;
 begin
-	if (x >= 0) and (x < SG(FWidth)) and (y >= 0) and (y < SG(FHeight)) then
-		Result := PBlur(UG(FData) + (UG(y) * FWidth + UG(x)) * SizeOf(TBlur))
-	else
-		Result := nil;
+  if (x >= 0) and (x < SG(FWidth)) and (y >= 0) and (y < SG(FHeight)) then
+    Result := PBlur(UG(FData) + (UG(y) * FWidth + UG(x)) * SizeOf(TBlur))
+  else
+    Result := nil;
 end;
 
 procedure TBlurArray.SetSize(const Width, Height: UG);
 begin
-	if (Width <> FWidth) or (Height <> FHeight) then
-	begin
-		FWidth := Width;
-		FHeight := Height;
-		FreeMem(FData);
-		GetMem(FData, Width * Height * SizeOf(TBlur));
-	end;
+  if (Width <> FWidth) or (Height <> FHeight) then
+  begin
+    FWidth := Width;
+    FHeight := Height;
+    FreeMem(FData);
+    GetMem(FData, Width * Height * SizeOf(TBlur));
+  end;
 end;
-
 
 function GetLength(const FloPoint: TFloPoint; const P: TPointDS): TFlo;
 begin
-	Result := Sqr(FloPoint.X - P.X) + Sqr(FloPoint.Y - P.Y);
+  Result := Sqr(FloPoint.X - P.X) + Sqr(FloPoint.Y - P.Y);
 end;
 
 function GetLength(const X, Y: SG; const P: TPointDS): TFlo;
 begin
-	Result := Sqr(X - P.X) + Sqr(Y - P.Y);
+  Result := Sqr(X - P.X) + Sqr(Y - P.Y);
 end;
 
 end.
+
