eb90193d5e0222ff22ba0b3897223552b269e93d
David Šafránek <safrad@email.cz>
2018-01-02 13:09:27 +0100

    Add benchmarks.

---
+++ Core/uSxMeasuredThread.pas
@@ -0,0 +1,62 @@
+unit uSxMeasuredThread;
+
+interface
+
+uses
+  uSxThread,
+  uTimeSpan,
+  uStopwatch;
+
+type
+  TSxMeasuredThread = class(TSxThread)
+  private
+    FStopwatch: TStopwatch;
+    function GetElapsed: TTimeSpan;
+  public
+    constructor Create(CreateSuspended: Boolean);
+    destructor Destroy; override;
+    procedure Execute; override;
+    procedure DoTerminate; override;
+    property Elapsed: TTimeSpan read GetElapsed;
+  end;
+
+implementation
+
+uses
+  SysUtils;
+
+{ TSxMeasuredThread }
+
+constructor TSxMeasuredThread.Create(CreateSuspended: Boolean);
+begin
+  inherited Create(CreateSuspended);
+  FStopwatch := TStopwatch.Create;
+end;
+
+destructor TSxMeasuredThread.Destroy;
+begin
+  FreeAndNil(FStopwatch);
+  inherited;
+end;
+
+procedure TSxMeasuredThread.DoTerminate;
+begin
+  if FStopwatch.IsRunning then
+    FStopwatch.Stop;
+
+  inherited DoTerminate;
+end;
+
+procedure TSxMeasuredThread.Execute;
+begin
+  FStopwatch.Start;
+
+  inherited Execute;
+end;
+
+function TSxMeasuredThread.GetElapsed: TTimeSpan;
+begin
+  Result := FStopwatch.Elapsed;
+end;
+
+end.
