a96e8d74cd6eee5c8d21b1f2a41baf561c422e4a
David Šafránek <safrad@email.cz>
2018-02-10 15:17:22 +0100

    Remove memory managers.

--- Core/uFirst.pas
+++ Core/uFirst.pas
@@ -6,28 +6,19 @@ interface
 
 uses
   Windows,
-  {$ifopt d+}
-//  SafeMMInstall,
-  {$else}
-	FastMM4,
-  {$endif}
   uTypes;
 
-{$ifopt d-}
 // Warning this unit can be used only in exe, not in dll/bpl
 // in some cases it is possible to use it with correctly set ImageBase of dll
 // Executable file is smaller
 {$SETPEFLAGS IMAGE_FILE_RELOCS_STRIPPED}
-{$endif}
 
 implementation
 
 initialization
 {$IFNDEF NoInitialization}
-	{$ifndef LINUX}
 	if IsRelease then
 		NoErrMsg := True;
-	{$endif}
 
 //	if IsDebug then
 //		ReportMemoryLeaksOnShutdown := True; // Can take long time for many unfreed objects
