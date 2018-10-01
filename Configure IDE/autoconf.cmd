2b17615733da3ed39a817e3a8cc8e89ba23c710d
David Šafránek <safrad@email.cz>
2018-10-01 12:29:08 +0200

    Move from root to "Configure IDE" folder.

--- Configure IDE/autoconf.cmd
+++ Configure IDE/autoconf.cmd
@@ -1,9 +1,10 @@
 rem run before Delphi IDE starts
 
-rem set user environment variable sx-library to current path
+rem set user environment variable sx-library to repository root
+pushd ..
 setx sx-library "%cd%"
-
 @IF NOT %ERRORLEVEL% EQU 0 PAUSE
+popd 
 
 SxConfDelphi.exe -l lib.txt -b bpl.txt
 
