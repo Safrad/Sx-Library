64d56f54f2146393506ab8b5bced025677b9e9af
David Šafránek <safrad@email.cz>
2018-10-07 07:11:33 +0200

    Fix unicode.

--- Core/uOutputFormat.pas
+++ Core/uOutputFormat.pas
@@ -437,7 +437,7 @@ begin
   end;
 
     //convert the array of char into an easy to access string
-    digits := PChar(Addr(floatRec.Digits[0]));
+    digits := PAnsiChar(Addr(floatRec.Digits[0]));
 
     if floatRec.Exponent > 0 then
     begin
