85dc1011d60179caf775221b2b2f6f968f38afe4
David Šafránek <safrad@email.cz>
2018-10-14 11:09:27 +0200

    Add PopAsyncTasks for optimization.

--- Core/uWorkerThread.pas
+++ Core/uWorkerThread.pas
@@ -25,7 +25,7 @@ type
 implementation
 
 uses
-  Windows, SysUtils, Classes, uSxXMLDocument;
+  Math, SysUtils, Classes, uSxXMLDocument;
 
 { TWorkerThread }
 
@@ -44,6 +44,8 @@ end;
 procedure TWorkerThread.Execute;
 var
   AsyncTask: TAsyncTask;
+  AsyncTasks: TAsyncTasks;
+  i: SG;
 begin
   inherited;
   if FUseCoInitialize then
@@ -52,56 +54,61 @@ begin
   try
     while FId < {FThreadPool.FRunThreads <=} FThreadPool.MaxThreads do // Read shared object
     begin
-      AsyncTask := FThreadPool.PopAsyncTask;
-      if AsyncTask <> nil then
+//      AsyncTask := FThreadPool.PopAsyncTask;
+      AsyncTasks := FThreadPool.PopAsyncTasks(Max(1, FThreadPool.RemainTaskCount div FThreadPool.MaxThreads div 10));
+      for i := 0 to Length(AsyncTasks) - 1 do
       begin
-        FThreadPool.WorkerStartWork;
-        try
-          AsyncTask.Thread := Self;
+        AsyncTask := AsyncTasks[i];
+        if AsyncTask <> nil then
+        begin
+          FThreadPool.WorkerStartWork;
           try
-            AsyncTask.Execute;
-          except
-            // Task failed
-            on E: Exception do
-            begin
-              try
-                AsyncTask.SynchronizedFail;
-              except
-                // No code
+            AsyncTask.Thread := Self;
+            try
+              AsyncTask.Execute;
+            except
+              // Task failed
+              on E: Exception do
+              begin
+                try
+                  AsyncTask.SynchronizedFail;
+                except
+                  // No code
+                end;
               end;
             end;
-          end;
-        finally
-          try
-            AsyncTask.SynchronizedFinish;
-          except
-            // No code
-          end;
-          FThreadPool.WorkerFinishWork;
-          if not FThreadPool.Working then
-          begin
-            if Assigned(FThreadPool.OnTasksFinished) then
+          finally
+            try
+              AsyncTask.SynchronizedFinish;
+            except
+              // No code
+            end;
+            FThreadPool.WorkerFinishWork;
+            if not FThreadPool.Working then
             begin
-              try
-                AsyncTask.Synchronize(FThreadPool.InternalTasksFinished);
-              except
-                // No code
+              if Assigned(FThreadPool.OnTasksFinished) then
+              begin
+                try
+                  AsyncTask.Synchronize(FThreadPool.InternalTasksFinished);
+                except
+                  // No code
+                end;
               end;
             end;
+            try
+              AsyncTask.Free;
+            except
+              // No code
+            end;
           end;
-          try
-            AsyncTask.Free;
-          except
-            // No code
-          end;
+        end
+        else
+        begin
+          // No task available, "ThreadPool.AddTask" can wake up thread
+          Suspend;
+  //          Exit; // Can be used for debbuging
+  //         Sleep(FThreadPool.RunThreads * LoopSleepTime); // Can be used for debbuging
         end;
-      end
-      else
-      begin
-        // No task available, "ThreadPool.AddTask" can wake up thread
-        Suspend;
-//          Exit; // Can be used for debbuging
-//         Sleep(FThreadPool.RunThreads * LoopSleepTime); // Can be used for debbuging
       end;
     end;
   finally
