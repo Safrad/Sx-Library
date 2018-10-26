6b27c29bee748601d4a64ebb96c7b51eff74abde
David Šafránek <safrad@email.cz>
2018-10-26 10:12:06 +0200

    Add Windows Management Instrumentation support.

---
+++ API/Windows/uWindowsManagementInstrumentation.pas
@@ -0,0 +1,47 @@
+unit uWindowsManagementInstrumentation;
+
+interface
+
+type
+  TWindowsManagementInstrumentation = class
+  public
+    class function GetWMIObject(const objectName: String): IDispatch;
+    class function GetDatabaseItem(const ADatabaseName, AColumnName: string): string;
+  end;
+
+implementation
+
+uses
+  ActiveX,
+  ComObj;
+
+class function TWindowsManagementInstrumentation.GetWMIObject(const objectName: String): IDispatch;
+var
+  chEaten: Integer;
+  BindCtx: IBindCtx;
+  Moniker: IMoniker;
+begin
+ 	Assert(CoInitializeEx(nil, COINIT_MULTITHREADED or COINIT_SPEED_OVER_MEMORY) <> S_FALSE);
+
+  OleCheck(CreateBindCtx(0, BindCtx));
+  OleCheck(MkParseDisplayName(BindCtx, StringToOleStr(objectName), chEaten, Moniker));
+  OleCheck(Moniker.BindToObject(BindCtx, nil, IDispatch, Result));
+end;
+
+class function TWindowsManagementInstrumentation.GetDatabaseItem(const ADatabaseName, AColumnName: string): string;
+var
+  objWMIService : OLEVariant;
+  colItems      : OLEVariant;
+  colItem       : OLEVariant;
+  oEnum         : IEnumvariant;
+  iValue        : LongWord;
+
+begin
+  objWMIService := GetWMIObject('winmgmts:\\localhost\root\cimv2');
+  colItems      := objWMIService.ExecQuery('SELECT ' + AColumnName +' FROM ' + ADatabaseName, 'WQL', 0);
+  oEnum         := IUnknown(colItems._NewEnum) as IEnumVariant;
+  if oEnum.Next(1, colItem, iValue) = 0 then
+    Result := colItem.Caption;
+end;
+
+end.
