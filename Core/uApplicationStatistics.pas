2f40eb1be617674c307b5a2c38fe3da20755df77
David Šafránek <safrad@email.cz>
2018-11-14 09:44:04 +0100

    Refactoring.

--- Core/uApplicationStatistics.pas
+++ Core/uApplicationStatistics.pas
@@ -4,18 +4,30 @@ interface
 
 uses
   uTypes,
+  uFile,
   uDIniFile,
   uTimeSpan;
 
 type
+  TRunStatistics = record
+    Count: U8;
+    TotalElapsedTimeInMs: U8;
+  end;
+
+  TSection = record
+    RunStatistics: TRunStatistics;
+    FileStatistics: TFileStatistics;
+  end;
+
   TApplicationStatistics = class
   strict private
-    FRunCount: UG;
+    FSection: TSection;
     FRunFirstTime: BG;
     FStartTime: U8;
     FElapsedTime: TTimeSpan;
     FTotalElapsedTime: TTimeSpan;
-    procedure RWData(const Save: BG);
+    procedure RWData(const ASave: BG);
+    procedure RWSection(const AIniFile: TDIniFile; const ASave: BG);
     function GetElapsedTime: TTimeSpan;
   public
     constructor Create;
@@ -23,14 +35,14 @@ type
 
     property ElapsedTime: TTimeSpan read GetElapsedTime;
     property TotalElapsedTime: TTimeSpan read FTotalElapsedTime;
-    property RunCount: UG read FRunCount;
+    property RunCount: U8 read FSection.RunStatistics.Count;
     property RunFirstTime: BG read FRunFirstTime;
   end;
 
 implementation
 
 uses
-	uFile, uProjectInfo, uMath, uMainTimer,
+	uProjectInfo, uMath, uMainTimer,
 	Windows;
 
 { TApplicationStatistics }
@@ -44,16 +56,14 @@ begin
   FTotalElapsedTime := TTimeSpan.Create;
 
   RWData(False);
-	Inc(FRunCount);
+	Inc(FSection.RunStatistics.Count);
 
-	Assert(FRunCount > 0);
-	FRunFirstTime := FRunCount <= 1;
+	Assert(FSection.RunStatistics.Count > 0);
+	FRunFirstTime := FSection.RunStatistics.Count <= 1;
 end;
 
 destructor TApplicationStatistics.Destroy;
 begin
-	FTotalElapsedTime.Ticks := FTotalElapsedTime.Ticks + FElapsedTime.Ticks;
-
   RWData(True);
 
   FTotalElapsedTime.Free;
@@ -68,30 +78,51 @@ begin
   Result := FElapsedTime;
 end;
 
-procedure TApplicationStatistics.RWData(const Save: BG);
 const
 	Section = 'Statistics';
-var
-  TotalElapsedTime: U8;
-begin
-	if Assigned(LocalMainIni) then
-	begin
-		if Save then
-  		LocalMainIni.WriteString(Section, 'Version', GetProjectInfo(piProductVersion));
 
-		LocalMainIni.RWNum(Section, 'RunCount', FRunCount, Save);
-    if Save then
-      TotalElapsedTime := Round(FTotalElapsedTime.Milliseconds)
-    else
-      TotalElapsedTime := 0;
-		LocalMainIni.RWNum(Section, 'RunTime', TotalElapsedTime, Save);
-    if Save = False then
-      FTotalElapsedTime.Milliseconds := TotalElapsedTime;
-		LocalMainIni.RWNum(Section, 'ReadCount', FileStatistics.ReadCount, Save);
-		LocalMainIni.RWNum(Section, 'ReadBytes', FileStatistics.ReadBytes, Save);
-		LocalMainIni.RWNum(Section, 'WriteCount', FileStatistics.WriteCount, Save);
-		LocalMainIni.RWNum(Section, 'WriteBytes', FileStatistics.WriteBytes, Save);
+procedure TApplicationStatistics.RWData(const ASave: BG);
+begin
+  if ASave then
+  begin
+    FSection.FileStatistics := FileStatistics;
+    FSection.RunStatistics.TotalElapsedTimeInMs := Round(FTotalElapsedTime.Milliseconds);
+  	if Assigned(LocalMainIni) then
+    begin
+      FSection.RunStatistics.TotalElapsedTimeInMs := Trunc(FTotalElapsedTime.Milliseconds + FElapsedTime.Milliseconds);
+      LocalMainIni.WriteString(Section, 'Version', GetProjectInfo(piProductVersion));
+      RWSection(LocalMainIni, True);
+    end;
+
+    // Backward compatibility
+  	if Assigned(MainIni) then
+      MainIni.DeleteSection(Section);
+  end
+  else
+  begin
+    FSection.RunStatistics.TotalElapsedTimeInMs := 0;
+    FSection.FileStatistics := FileStatistics;
+
+    // Backward compatibility
+  	if Assigned(MainIni) then
+      RWSection(MainIni, False);
+
+  	if Assigned(LocalMainIni) then
+      RWSection(LocalMainIni, False);
+
+    FTotalElapsedTime.Milliseconds := FSection.RunStatistics.TotalElapsedTimeInMs;
+    FileStatistics := FSection.FileStatistics;
 	end;
+end;
+
+procedure TApplicationStatistics.RWSection(const AIniFile: TDIniFile; const ASave: BG);
+begin
+  AIniFile.ReadAndIncrementOrWrite(Section, 'RunCount', FSection.RunStatistics.Count, ASave);
+  AIniFile.ReadAndIncrementOrWrite(Section, 'RunTime', FSection.RunStatistics.TotalElapsedTimeInMs, ASave);
+  AIniFile.ReadAndIncrementOrWrite(Section, 'ReadCount', FSection.FileStatistics.ReadCount, ASave);
+  AIniFile.ReadAndIncrementOrWrite(Section, 'ReadBytes', FSection.FileStatistics.ReadBytes, ASave);
+  AIniFile.ReadAndIncrementOrWrite(Section, 'WriteCount', FSection.FileStatistics.WriteCount, ASave);
+  AIniFile.ReadAndIncrementOrWrite(Section, 'WriteBytes', FSection.FileStatistics.WriteBytes, ASave);
 end;
 
 end.
