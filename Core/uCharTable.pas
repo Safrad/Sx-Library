d446dcf4600b4e6b97a7e4b3727c35f6cbda6b95
David Šafránek <safrad@email.cz>
2019-02-27 10:59:19 +0100

    Add ctPercent.

--- Core/uCharTable.pas
+++ Core/uCharTable.pas
@@ -4,9 +4,9 @@ interface
 
 type
 	TCharType = (
-		{ctSpace, ctTab,} ctLetter, ctLastLetter, ctBlank, ctReturn, ctDollar, ctIllegal, ctNumber, ctNumberPrefix,
+		ctLetter, ctBlank, ctReturn, ctDollar, ctIllegal, ctNumber, ctNumberPrefix,
 		ctPlus, ctMinus, ctExp, ctMul, ctDiv, ctOpen, ctClose,
-		ctPoint, ctComma, ctSemicolon, ctPercent);
+		ctPoint, ctComma, ctSemicolon);
 
 	TCharTable = array[AnsiChar] of TCharType;
 var
@@ -30,7 +30,7 @@ begin
 		CharCR, CharLF: CharTable[c] := ctReturn;
 		'a'..'z', 'A'..'Z', '_'{, #$80..#$ff}: CharTable[c] := ctLetter;
 		'0'..'9': CharTable[c] := ctNumber;
-		{'!',} '#', '$' {'a'..'z', 'A'..'Z'}: CharTable[c] := ctNumberPrefix;
+		{'!',} '#', '$', '%' {'a'..'z', 'A'..'Z'}: CharTable[c] := ctNumberPrefix;
 		'+': CharTable[c] := ctPlus;
 		'-': CharTable[c] := ctMinus;
 		'^': CharTable[c] := ctExp;
@@ -41,7 +41,6 @@ begin
 		'.': CharTable[c] := ctPoint;
 		',': CharTable[c] := ctComma;
 		';': CharTable[c] := ctSemicolon;
-		'%': CharTable[c] := ctPercent;
 		else
 			CharTable[c] := ctIllegal;
 		end;
@@ -55,34 +54,9 @@ begin
 		Result := CharTable[AnsiChar(c)]
 	{$ifdef UNICODE}
 	else
-		Result := ctIllegal;
+		Result := ctLetter; // i. e. Greece alphabet
 	{$endif}
 end;
-(*
-procedure FillCharsTable;
-var c: AnsiChar;
-begin
-	// Make Char Table
-	for c := Low(c) to High(c) do
-		case c of
-		' ': CharsTable[c] := ctSpace;
-		'a'..'z', 'A'..'Z', '_': CharsTable[c] := ctLetter;
-		'0'..'9', '!', '#', '$', '%' {'a'..'z', 'A'..'Z'}: CharsTable[c] := ctNumber;
-		'+': CharsTable[c] := ctPlus;
-		'-': CharsTable[c] := ctMinus;
-		'^': CharsTable[c] := ctExp;
-		'*': CharsTable[c] := ctMul;
-		'/': CharsTable[c] := ctDiv;
-		'(': CharsTable[c] := ctOpen;
-		')': CharsTable[c] := ctClose;
-		'.', ',': CharsTable[c] := ctNumber;
-		else
-			if (c = DecimalSeparator[1]) or (c = ThousandSeparator[1]) then
-				CharsTable[c] := ctNumber
-			else
-				CharsTable[c] := ctIllegal;
-		end;
-end;*)
 
 initialization
 {$IFNDEF NoInitialization}
