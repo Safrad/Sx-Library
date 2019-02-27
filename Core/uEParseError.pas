8badb936495797d5908838090b0bba530865e04c
David Šafránek <safrad@email.cz>
2019-02-27 11:02:33 +0100

    Add constructors.

--- Core/uEParseError.pas
+++ Core/uEParseError.pas
@@ -8,9 +8,12 @@ uses
 type
   EParseError = class(Exception)
   private
+    function CharsToString(const AChars: array of Char): string;
     function TokensToString(const ATokens: array of string): string;
   public
+    constructor Create(const AExpectedChars: array of Char; const AActualChar: Char); overload;
     constructor Create(const AExpectedTokens: array of string; const AActualToken: string); overload;
+    constructor Create(const AMessage: string; const AExpectedChars: array of Char; const AActualChar: Char); overload;
     constructor Create(const AMessage: string; const AExpectedTokens: array of string; const AActualToken: string); overload;
   end;
 
@@ -33,6 +36,16 @@ begin
   Message := AMessage + ' ' + TokensToString(AExpectedTokens) + ' expected but token ' + AddSingleQuoteF(AActualToken) + ' found.';
 end;
 
+constructor EParseError.Create(const AExpectedChars: array of Char; const AActualChar: Char);
+begin
+  Create('Parse error:', AExpectedChars, AActualChar);
+end;
+
+constructor EParseError.Create(const AMessage: string; const AExpectedChars: array of Char; const AActualChar: Char);
+begin
+  Message := AMessage + ' ' + CharsToString(AExpectedChars) + ' expected but token ' + AddSingleQuoteF(AActualChar) + ' found.';
+end;
+
 function EParseError.TokensToString(const ATokens: array of string): string;
 var
   i: SG;
@@ -44,6 +57,21 @@ begin
     if i = Length(ATokens) - 2 then
       Result := Result + ' or '
     else if i < Length(ATokens) - 1 then
+      Result := Result + ', ';
+  end;
+end;
+
+function EParseError.CharsToString(const AChars: array of Char): string;
+var
+  i: SG;
+begin
+  Result := '';
+  for i := 0 to Length(AChars) - 1 do
+  begin
+    Result := Result + AddSingleQuoteF(AChars[i]);
+    if i = Length(AChars) - 2 then
+      Result := Result + ' or '
+    else if i < Length(AChars) - 1 then
       Result := Result + ', ';
   end;
 end;
