927baaf09b458518b000959629661113177e560d
David Šafránek <safrad@email.cz>
2019-12-02 11:07:24 +0100

    Change ByteOrderMark from AnsiChar to Byte.

--- Files/uFileCharset.pas
+++ Files/uFileCharset.pas
@@ -7,45 +7,68 @@ uses
 
 type
 	TFileCharset = (fcUnknown, fcAnsi, fcUTF8, fcUTF16BE, fcUTF16LE { Windows } , fcUTF32BE, fcUTF32LE
-		{ Windows } , fcUTF7a, fcUTF7b, fcUTF7c, fcUTF7d, fcUTF1, fcUTFEBCDIC, fcSCSU, fcBOCU1,
-		fcBOCU1b, fcGB18030);
+		{ Windows } , fcUTF7a, fcUTF7b, fcUTF7c, fcUTF7d, fcUTF1, fcUTFEBCDIC, fcSCSU, fcBOCU1b, fcBOCU1,
+		fcGB18030);
 
 const
+	DefaultFileCharset = fcUTF8;
   MaxByteOrderMarkSize = 4;
-  ByteOrderMarks: array [TFileCharset] of RawByteString = (
-    '',
-    '',
-    RawByteString(#$EF + #$BB + #$BF),
-    RawByteString(#$FE + #$FF),
-    RawByteString(#$FF + #$FE),
-    RawByteString(#$00 + #$00 + #$FE + #$FF),
-    RawByteString(#$FF + #$FE + #$00 + #$00),
-    RawByteString(#$2B + #$2F + #$76 + #$38),
-    RawByteString(#$2B + #$2F + #$76 + #$39),
-    RawByteString(#$2B + #$2F + #$76 + #$2B),
-    RawByteString(#$2B + #$2F + #$76 + #$2F),
-    RawByteString(#$F7 + #$64 + #$4C),
-    RawByteString(#$DD + #$73 + #$66 + #$73),
-    RawByteString(#$0E + #$FE + #$FF),
-    RawByteString(#$FB + #$EE + #$28),
-    RawByteString(#$FB + #$EE + #$28 + #$FF),
-    RawByteString(#$84 + #$31 + #$95 + #$33)
+type
+  TByteOrderMark = array of U1;
+const
+  ByteOrderMarks: array [TFileCharset] of TByteOrderMark = (
+    [],
+    [],
+    [$EF, $BB, $BF],
+    [$FE, $FF],
+    [$FF, $FE],
+    [$00, $00, $FE, $FF],
+    [$FF, $FE, $00, $00],
+    [$2B, $2F, $76, $38],
+    [$2B, $2F, $76, $39],
+    [$2B, $2F, $76, $2B],
+    [$2B, $2F, $76, $2F],
+    [$F7, $64, $4C],
+    [$DD, $73, $66, $73],
+    [$0E, $FE, $FF],
+    [$FB, $EE, $28, $FF],
+    [$FB, $EE, $28],
+    [$84, $31, $95, $33]
   );
 
   CharsetSize: array [TFileCharset] of SG = (0, 1, 1{1..4}, 2, 2, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1); // [Bytes / Char]
 
-function FindFileCharset(const AByteOrderMark: RawByteString): TFileCharset;
+function FindFileCharset(const AHeader: TByteOrderMark): TFileCharset;
 
 implementation
 
-function FindFileCharset(const AByteOrderMark: RawByteString): TFileCharset;
+function ContainsByteOrderMark(const AHeader, AByteOrderMark: TByteOrderMark): BG;
+var
+  i: SG;
+begin
+  if Length(AHeader) < Length(AByteOrderMark) then
+    Result := False
+  else
+  begin
+    for i := 0 to Length(AByteOrderMark) - 1 do
+    begin
+      if AHeader[i] <> AByteOrderMark[i] then
+      begin
+        Result := False;
+        Exit;
+      end;
+    end;
+    Result := True;
+  end;
+end;
+
+function FindFileCharset(const AHeader: TByteOrderMark): TFileCharset;
 var
   Charset: TFileCharset;
 begin
 	for Charset := fcUTF8 to High(Charset) do
 	begin
-    // TODO: Optimize
-		if Copy(AByteOrderMark, 1, Length(ByteOrderMarks[Charset])) = ByteOrderMarks[Charset] then
+		if ContainsByteOrderMark(AHeader, ByteOrderMarks[Charset]) then
 		begin
       Result := Charset;
       Exit;
