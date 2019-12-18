96e49d469289d4e2ee1bfbefdc4ec10cef06f295
David Šafránek <safrad@email.cz>
2019-12-18 14:59:41 +0100

    Classify files.

--- Test/LibTest.dpr
+++ Test/LibTest.dpr
@@ -8,62 +8,78 @@ uses
   uTestsApplication,
   uEscapeTest in '..\Core\uEscapeTest.pas',
   uCharsetTest in '..\Core\uCharsetTest.pas',
-  uDelphiTest in '..\Core\uDelphiTest.pas',
-  uFileTest in '..\Core\uFileTest.pas',
-  uFilesTest in '..\Core\uFilesTest.pas',
-  uGeometry2DTest in '..\Geometry\uGeometry2DTest.pas',
-  uPolygon2DTest in '..\Geometry\uPolygon2DTest.pas',
   uStringsTest in '..\Core\uStringsTest.pas',
   uCompareTest in '..\Core\uCompareTest.pas',
   uFindTest in '..\Core\uFindTest.pas',
   uOutputFormatTest in '..\Core\uOutputFormatTest.pas',
   uBackupTest in '..\Core\uBackupTest.pas',
   uObjectFactoryTest in '..\Core\uObjectFactoryTest.pas',
-  uPictureFactoryTest in '..\GUI\uPictureFactoryTest.pas',
-  uWebUpdateTest in '..\GUI\uWebUpdateTest.pas',
-  uProjectOptionsTest in '..\Core\uProjectOptionsTest.pas',
-  uThreadPoolTest in '..\Core\uThreadPoolTest.pas',
   uDivideSpaceTest in '..\Core\uDivideSpaceTest.pas',
-  uCSVFileTest in '..\Core\uCSVFileTest.pas',
-  uRegionalTest in '..\Core\uRegionalTest.pas',
   uMathTest in '..\Core\uMathTest.pas',
-  uCPUTest in '..\Core\uCPUTest.pas',
-  uMathExpressionParserTest in '..\Parser\uMathExpressionParserTest.pas',
-  uArgumentsForTest in '..\Arguments\uArgumentsForTest.pas',
-  uArgumentsTest in '..\Arguments\uArgumentsTest.pas',
-  uLoadDllTest in 'DllForTest\uLoadDllTest.pas',
   uPermutationTest in '..\Core\uPermutationTest.pas',
   uStopwatchTest in '..\Core\uStopwatchTest.pas',
-  uWavePlayerTest in '..\Sound\Players\uWavePlayerTest.pas',
   uTicksTest in '..\Core\uTicksTest.pas',
-  uColorSequenceTest in '..\GUI\uColorSequenceTest.pas',
-  uRandomGeneratorTest in '..\RandomGenerator\uRandomGeneratorTest.pas',
   uMainTimerTest in '..\Core\uMainTimerTest.pas',
-  uDTimerTest in '..\GUI\VCL\Components\uDTimerTest.pas',
-  uSxThreadTimerTest in '..\Core\uSxThreadTimerTest.pas',
   uTextMacroTest in '..\Core\uTextMacroTest.pas',
   uStartupEnvironmentTest in '..\Core\uStartupEnvironmentTest.pas',
   uCyclicRedundancyCheckTest in '..\Core\uCyclicRedundancyCheckTest.pas',
-  uSplitFileTest in '..\Core\uSplitFileTest.pas',
-  uRegionalCodePageTest in '..\Regional\uRegionalCodePageTest.pas',
-  uWaveTest in '..\Sound\SoundItems\uWaveTest.pas',
-  uTimeExpressionParserTest in '..\Parser\uTimeExpressionParserTest.pas',
+  uIntegerListTest in '..\Core\uIntegerListTest.pas',
   uSxObjectListTest in '..\Core\uSxObjectListTest.pas',
   uSxStringListTest in '..\Core\uSxStringListTest.pas',
-  uExternalApplicationTest in '..\Core\uExternalApplicationTest.pas',
-  uPipedExternalApplicationTest in '..\Core\uPipedExternalApplicationTest.pas',
+
+  uRandomGeneratorTest in '..\RandomGenerator\uRandomGeneratorTest.pas',
+
+  uArgumentsForTest in '..\Arguments\uArgumentsForTest.pas',
+  uArgumentsTest in '..\Arguments\uArgumentsTest.pas',
+
+  uGeometry2DTest in '..\Geometry\uGeometry2DTest.pas',
+  uPolygon2DTest in '..\Geometry\uPolygon2DTest.pas',
+
+  uFileTest in '..\Files\uFileTest.pas',
+  uFilesTest in '..\Files\uFilesTest.pas',
+  uSplitFileTest in '..\Files\uSplitFileTest.pas',
   uRawFileTest in '..\Files\uRawFileTest.pas',
   uTextFileTest in '..\Files\uTextFileTest.pas',
-  uDBitmapTest in '..\GUI\uDBitmapTest.pas',
+  uCSVFileTest in '..\Files\Database\uCSVFileTest.pas',
+  uDBFTest in '..\Files\Database\uDBFTest.pas',
+
+  uMathExpressionParserTest in '..\Parser\uMathExpressionParserTest.pas',
+  uTimeExpressionParserTest in '..\Parser\uTimeExpressionParserTest.pas',
+
   uPercentFormatterTest in '..\Formatter\uPercentFormatterTest.pas',
+
+  uSxThreadTimerTest in '..\ThreadPool\uSxThreadTimerTest.pas',
+  uThreadPoolTest in '..\ThraedPool\uThreadPoolTest.pas',
+
+  uRegionalTest in '..\Regional\uRegionalTest.pas',
+  uRegionalCodePageTest in '..\Regional\uRegionalCodePageTest.pas',
+
+  uExternalApplicationTest in '..\ExternalApplication\uExternalApplicationTest.pas',
+  uPipedExternalApplicationTest in '..\ExternalApplication\uPipedExternalApplicationTest.pas',
+
+  uCPUTest in '..\Hardware\uCPUTest.pas',
+
+  uWavePlayerTest in '..\Sound\Players\uWavePlayerTest.pas',
+  uWaveTest in '..\Sound\SoundItems\uWaveTest.pas',
+
+  uGraphicObjectsRendererTest in '..\Renderer\uGraphicObjectsRendererTest.pas',
+
+  uSVGReaderTest in '..\SVG\uSVGReaderTest.pas',
+
+  uPictureFactoryTest in '..\GUI\uPictureFactoryTest.pas',
+  uWebUpdateTest in '..\GUI\uWebUpdateTest.pas',
+  uColorSequenceTest in '..\GUI\uColorSequenceTest.pas',
+  uDBitmapTest in '..\GUI\uDBitmapTest.pas',
   uDesktopWindowTest in '..\GUI\uDesktopWindowTest.pas',
   uTaskbarWindowTest in '..\GUI\uTaskbarWindowTest.pas',
   uScreenTest in '..\GUI\uScreenTest.pas',
-  uScreenMode in '..\GUI\uScreenMode.pas',
-  uGraphicObjectsRendererTest in '..\Renderer\uGraphicObjectsRendererTest.pas',
-  uSVGReaderTest in '..\SVG\uSVGReaderTest.pas',
-  uIntegerListTest in '..\Core\uIntegerListTest.pas',
-  uDBFTest in '..\Core\uDBFTest.pas';
+
+  uDTimerTest in '..\GUI\VCL\Components\uDTimerTest.pas',
+
+  uDelphiTest in '..\Delphi IDE\uDelphiTest.pas',
+  uProjectOptionsTest in '..\Delphi IDE\uProjectOptionsTest.pas',
+
+  uLoadDllTest in 'DllForTest\uLoadDllTest.pas';
 
 {$R *.RES}
 
